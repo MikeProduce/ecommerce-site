4ad2ce20c80ba04f1e6ed6b3d8a153fb537cab00
MikeProduce <Gomez_miguelomar@yahoo.com>
2023-08-28 18:59:52 -0500

    removed pages pay

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { createBrowserRouter, createRoutesFromElements, Route, Link, Outlet, RouterProvider } from 'react-router-dom'
 import { Home } from './Pages/home.jsx'
-import Pay from "./Pages/Pay.jsx";
 import { Clothing } from './Pages/Clothing'
 import { Other } from './Pages/Other'
 import { Electronics } from './Pages/Electronics'
@@ -20,7 +19,6 @@ function App() {
     createRoutesFromElements(
       <Route path='/' element={<Navbar />}>
         <Route index element={<Home />} />
-        <Route path="/Pay" element={<Pay />} />
         <Route path="/Clothing" element={<Clothing />} />
         <Route path="/CheckOrder" element={<CheckOrder />} />
         <Route path="/Other" element={<Other />} />
