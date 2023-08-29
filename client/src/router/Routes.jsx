a70c5efa437c060c79e7131c94557f6643e92546
MikeProduce <Gomez_miguelomar@yahoo.com>
2023-08-29 14:43:06 -0500

    seeing if brackets makes the difference

--- client/src/router/Routes.jsx
+++ client/src/router/Routes.jsx
@@ -2,21 +2,21 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import Home from '../Pages/home'
-import Cart from '../Pages/cart/Cart';
-import ClothingPage from '../Pages/Clothing';
-import OtherPage from '../Pages/Other';
-import Electronics from '../Pages/Electronics';
-import Navbar from '../components/Navbar';
-import Login from '../Pages/login/Login';
-import SignUp from '../Pages/sign-up/SignUp';
+import { Home } from '../Pages/home'
+import { Cart } from '../Pages/cart/Cart';
+import { Clothing } from '../Pages/Clothing';
+import { Other } from '../Pages/Other';
+import { Electronics } from '../Pages/Electronics';
+import { Navbar } from '../components/Navbar';
+import { Login } from '../Pages/login/Login';
+import { SignUp } from '../Pages/sign-up/SignUp';
 
 export const Routes = (
     <Route path="/" element={<Navbar />}>
         <Route index element={<Home />} />
         <Route path="/Cart" element={<Cart />} />
-        <Route path="/Clothing" element={<ClothingPage />} />
-        <Route path="/Other" element={<OtherPage />} />
+        <Route path="/Clothing" element={<Clothing />} />
+        <Route path="/Other" element={<Other />} />
         <Route path="/Electronics" element={<Electronics />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/SignUp" element={<SignUp />} />
