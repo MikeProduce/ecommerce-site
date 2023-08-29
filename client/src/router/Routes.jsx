29158fc05091e8d07cb9b496629d1cc904e7f3ad
MikeProduce <Gomez_miguelomar@yahoo.com>
2023-08-29 14:34:38 -0500

    changed the routes location

--- client/src/router/Routes.jsx
+++ client/src/router/Routes.jsx
@@ -2,14 +2,14 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 
-import Home from '../src/Pages/home'
-import Cart from '../src/Pages/cart/Cart';
-import ClothingPage from '../src/Pages/Clothing';
-import OtherPage from '../src/Pages/Other';
-import Electronics from '../src/Pages/Electronics';
-import Navbar from '../src/components/Navbar';
-import Login from '../src/Pages/login/Login';
-import SignUp from '../src/Pages/sign-up/SignUp';
+import Home from '../Pages/home'
+import Cart from '../Pages/cart/Cart';
+import ClothingPage from '../Pages/Clothing';
+import OtherPage from '../Pages/Other';
+import Electronics from '../Pages/Electronics';
+import Navbar from '../components/Navbar';
+import Login from '../Pages/login/Login';
+import SignUp from '../Pages/sign-up/SignUp';
 
 export const Routes = (
     <Route path="/" element={<Navbar />}>
