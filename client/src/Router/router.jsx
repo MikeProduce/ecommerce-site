f812c7081ecfe6637828d57550a1675f3e2903cb
MikeProduce <Gomez_miguelomar@yahoo.com>
2023-08-31 09:18:07 -0500

    changed to double quotes ?

--- client/src/Router/router.jsx
+++ client/src/Router/router.jsx
@@ -1,19 +1,21 @@
-import { createBrowserRouter } from 'react-router-dom';
-import App from '../App';
-import ErrorPage from '../Router/error-page.jsx';
-import Home from '../pages/home/Home';
-import Cart from '../pages/cart/Cart';
-import Clothing from '../pages/Clothing';
-import Other from '../pages/Other';
-import Electronics from '../pages/Electronics';
+import { createBrowserRouter } from "react-router-dom";
+
+
+import App from "../App";
+import ErrorPage from "../Router/error-page";
+import Home from "../pages/home/Home";
+import Cart from "../pages/cart/Cart";
+import Clothing from "../pages/Clothing";
+import Other from "../pages/Other";
+import Electronics from "../pages/Electronics";
 import LoginUser from "../pages/login/loginPage";
-import SignUp from '../pages/sign-up/SignUp';
+import SignUp from "../pages/sign-up/SignUp";
 
 // Define your routes and their associated components
 export const router = createBrowserRouter([
     {
         // Set the path to the root URL
-        path: '/',
+        path: "/",
         // Use the App component as the root element of the router
         element: <App />,
 
@@ -25,27 +27,27 @@ export const router = createBrowserRouter([
                 element: <Home />,
             },
             {
-                path: '/Login',
+                path: "/Login",
                 element: <LoginUser />,
             },
             {
-                path: '/Cart',
+                path: "/Cart",
                 element: <Cart />,
             },
             {
-                path: '/Clothing',
+                path: "/Clothing",
                 element: <Clothing />,
             },
             {
-                path: '/Other',
+                path: "/Other",
                 element: <Other />,
             },
             {
-                path: '/Electronics',
+                path: "/Electronics",
                 element: <Electronics />,
             },
             {
-                path: '/SignUp',
+                path: "/SignUp",
                 element: <SignUp />,
             },
         ],
