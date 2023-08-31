df3db17708d88adfe54bd479fe386f346c5c7122
MikeProduce <Gomez_miguelomar@yahoo.com>
2023-08-31 09:02:33 -0500

    what about now ?

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { Provider } from 'react-redux';
+import "./index.css";
+import router from "./Router/router"
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "react-auth-kit";
-import router from "./Router/router"
+import { Provider } from 'react-redux';
 
 import { store } from './app/store';
-import "./index.css";
 
 createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <React.StrictMode>
       <AuthProvider
-        authType="cookie"
-        authName="access_token"
+        authType={"cookie"}
+        authName={"access_token"}
         cookieDomain={window.location.hostname}
         cookieSecure={false}
       >
