3d7bf45cd38b9f27f75002f7a1743a4f8e24de04
MikeProduce <Gomez_miguelomar@yahoo.com>
2023-08-31 09:03:17 -0500

    forgot to save these

--- client/src/Router/router.jsx
+++ client/src/Router/router.jsx
@@ -1,14 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
-
-import Home from '../Pages/home/Home';
-import Cart from '../Pages/cart/Cart';
-import Clothing from '../Pages/Clothing';
-import Other from '../Pages/Other';
-import Electronics from '../Pages/Electronics';
-import LoginUser from "../pages/login/Login";
-import SignUp from '../Pages/sign-up/SignUp';
 import App from '../App';
 import ErrorPage from './error-page';
+import Home from '../pages/home/Home';
+import Cart from '../pages/cart/Cart';
+import Clothing from '../pages/Clothing';
+import Other from '../pages/Other';
+import Electronics from '../pages/Electronics';
+import LoginUser from "../pages/login/loginPage";
+import SignUp from '../pages/sign-up/SignUp';
 
 // Define your routes and their associated components
 export const router = createBrowserRouter([
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,7 +5,6 @@ import router from "./Router/router"
 import { RouterProvider } from "react-router-dom";
 import { AuthProvider } from "react-auth-kit";
 import { Provider } from 'react-redux';
-
 import { store } from './app/store';
 
 createRoot(document.getElementById("root")).render(
