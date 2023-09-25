f310c4631791de05b4fda6ce0817681d5d8954cc
MikeProduce <Gomez_miguelomar@yahoo.com>
2023-09-25 14:51:59 -0500

    deleted item

--- client/src/components/Button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-export const Button = (props) => {
-  return (
-    <button  onClick={props.onClick} className='bg-gray-800 text-white p-2 rounded-md hover:bg-opacity-50 focus:outline-none focus:shadow-outline flex-1'>{props.children}</button>
-  );
-}
-
