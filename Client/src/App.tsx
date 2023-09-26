28654a371312d4ca5d0d25bd74173755483c4eb8
MikeProduce <105679025+MikeProduce@users.noreply.github.com>
2023-09-26 09:09:56 -0500

    started on the navigation-bar.

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,14 +1,11 @@
-import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
+import { Navbar } from "./navigation/Navbar";
 
 export default function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <Link to="/login-user">Login</Link>
+    <div className="">
+      <Navbar />
       <Outlet />
-    </>
+    </div>
   )
 }
